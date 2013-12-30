050b0627cbedc580ed463058f03bf90cc8651bd9
james <james@datagraph.org>
2013-12-30 12:34:05 +0000

    changes to accommodate use with divergent store site and host names

--- sparql-graph-store-http-protocol/GET-direct-nquads.sh
+++ sparql-graph-store-http-protocol/GET-direct-nquads.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
 
 
-curl -f -s -X GET \
+${CURL} -f -s -X GET \
      -H "Accept: application/n-quads" \
-     ${STORE_NAMED_GRAPH}?auth_token=${STORE_TOKEN} \
+     ${STORE_NAMED_GRAPH_URL}?auth_token=${STORE_TOKEN} \
    | rapper -q --input nquads --output nquads /dev/stdin - | tr -s '\n' '\t' \
    | fgrep '"named object"' | fgrep "${STORE_NAMED_GRAPH}" \
    | tr -s '\t' '\n' | wc -l | fgrep -q 1

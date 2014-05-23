b13e86eaed6900c343d9f69884fbfbcef11e16f3
james anderson <james@dydra.com>
2014-05-23 21:12:43 +0200

    replace json_reformat with jq for targeted content tests

--- sparql-protocol/GET-srj+srx.sh
+++ sparql-protocol/GET-srj+srx.sh
@@ -5,5 +5,4 @@
 curl -f -s -X GET\
      -H 'Accept: application/sparql-results+json,application/sparql-results+xml,*/*;q=0.9' \
      ${STORE_URL}/${STORE_ACCOUNT}/${STORE_REPOSITORY}?'query=select%20count(*)%20where%20%7b?s%20?p%20?o%7d&'auth_token=${STORE_TOKEN} \
-   | json_reformat -m \
-   | egrep -q -s '"bindings".*"COUNT1".*"value":"1"'
+   | jq '.results.bindings[] | .[].value' | fgrep -q '"1"'

b67f3c86c6a8d6851a2e201781eb0d40dd647477
james anderson <james@dydra.com>
2014-12-12 21:43:36 +0100

    add the aggregation tests to push

---
+++ extensions/sparql-protocol/temporal-data/aggregates.sh
@@ -0,0 +1,57 @@
+#! /bin/bash
+
+# exercise aggregation over temporal data
+
+$CURL -w "%{http_code}\n" -f -s -S -X PUT \
+     -H "Content-Type: application/turtle" \
+     --data-binary @- \
+     -u "${STORE_TOKEN}:" \
+     "${GRAPH_STORE_URL}?default" <<EOF \
+    | egrep -q "$STATUS_PUT_SUCCESS"
+@prefix ex: <http://example.com/> .
+@prefix xsd: <http://www.w3.org/2001/XMLSchema-datatypes#> .
+
+_:a ex:start "2014-01-01T00:00:00Z"^^xsd:dateTime ;
+  ex:end "2014-01-01T06:00:00Z"^^xsd:dateTime .
+
+_:b ex:start "2014-01-01T00:00:00Z"^^xsd:dateTime ;
+  ex:end "2014-01-02T00:00:00Z"^^xsd:dateTime .
+
+_:c ex:start "2014-01-01T00:00:00Z"^^xsd:dateTime ;
+  ex:end "2014-02-01T00:00:00Z"^^xsd:dateTime .
+
+_:b ex:start "2014-01-01T00:00:00Z"^^xsd:dateTime ;
+  ex:end "2015-01-01T00:00:00Z"^^xsd:dateTime .
+EOF
+
+
+${CURL} -f -s -S -X POST \
+     -H "Content-Type: application/sparql-query" \
+     -H "Accept: application/sparql-results+json" \
+     --data-binary @- \
+     -u "${STORE_TOKEN}:" \
+     "${SPARQL_URL}" <<EOF \
+ | jq '.results.bindings[] | .[].value' | fgrep -q 'true'
+
+prefix xsd: <http://www.w3.org/2001/XMLSchema-datatypes>
+prefix ex: <http://example.com/>
+
+select (((min(?endDate) = '2014-01-01T06:00:00Z'^^xsd:dateTime) &&
+         (max(?endDate) = '2015-01-01T00:00:00Z'^^xsd:dateTime) &&
+         # not possible to add dates
+         # (avg(?startDate) = '2014-01-01T00:00:00Z'^^xsd:dateTime) &&
+         (sum(?duration) = xsd:dayTimeDuration('P397DT6H')) &&
+         (min(?duration) = xsd:dayTimeDuration('PT6H'))  &&
+         (max(?duration) = xsd:dayTimeDuration('P365D')) &&
+         (avg(?duration) = xsd:dayTimeDuration('P99DT7H30M'))
+         )
+        as ?ok)
+where {
+  ?s ex:start ?startDate ;
+    ex:end ?endDate .
+  bind ((?endDate - ?startDate) as ?duration)
+}
+EOF
+
+# this would signal an error
+# (spocq.e:+ ({xsd}dateTime "2014-01-01T00:00:00Z") ({xsd}dateTime "2014-01-01T00:00:00Z"))

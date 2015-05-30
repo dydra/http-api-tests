a9295170fb4957e79a8ed19ecfab1f61e5fb3504
james anderson <james@dydra.com>
2015-05-30 23:48:54 +0200

    [skip ci] corrections to prospective request values tests

--- extensions/sparql-protocol/values/inline-update.sh
+++ extensions/sparql-protocol/values/inline-update.sh
@@ -3,10 +3,18 @@
 # exercise the values extension for a query in-line with the request
 
 curl_sparql_update "--data-urlencode" "@-" <<EOF \
- | jq '.results.bindings[] | .[].value' | fgrep -q "BSS84"
-update=sinsert { ?name <http://example.org/code> ?code }
+ | jq '.boolean' | fgrep -q 'true'
+update=insert { ?name <http://example.org/code> ?code }
 where {
   }
-&values=($name $code) { ("BUK7Y98-80E" "one") ("PH3330L" "two") ("BSS84" "three") }
+&values=($name $code)
+        { ("BUK7Y98-80E" "one")
+          ("PH3330L" "two")
+          ("BSS84" "three") }
 EOF
 
+curl_sparql_request "--data-urlencode" "@-" <<EOF \
+ | jq '.results.bindings[] | .[].value' | fgrep -q "BSS84"
+query=select ?name
+where { ?name ?p ?o }
+EOF
--- extensions/sparql-protocol/values/stored-update.sh
+++ extensions/sparql-protocol/values/stored-update.sh
@@ -4,7 +4,13 @@
 # nb. the query "values-update-test" must exist in the account
 
 SPARQL_URL="${STORE_URL}/${STORE_ACCOUNT}/${STORE_REPOSITORY}-write/values-update-test" \
-curl_sparql_request "--data-urlencode" "@-" -X ECHO <<EOF \
- | jq '.results.bindings[] | .[].value' | fgrep -q "BSS84"
+curl_sparql_update "--data-urlencode" "@-" <<EOF \
+ | jq '.boolean' | fgrep -q 'true'
 values=values=($name $code) { ("BUK7Y98-80E" "one") ("PH3330L" "two") ("BSS84" "three") }
+EOF
+
+curl_sparql_request "--data-urlencode" "@-" <<EOF \
+ | jq '.results.bindings[] | .[].value' | fgrep -q "BSS84"
+query=select ?name
+where { ?name ?p ?o }
 EOF

542f9221e1657df9cf8d7edad04903969593ee05
james anderson <james@dydra.com>
2015-06-28 21:03:44 +0200

    [skip ci] revise PUT-ntriples+nquads-graph.sh to accumulate all content and require it to be in the given target graph

--- sparql-graph-store-http-protocol/PUT-ntriples+nquads-graph.sh
+++ sparql-graph-store-http-protocol/PUT-ntriples+nquads-graph.sh
@@ -15,8 +15,8 @@ curl_graph_store_update -X PUT \
 EOF
 
 
-curl_graph_store_update -X PUT \
-     -H "Content-Type: application/n-triples" \
+curl_graph_store_update -X POST \
+     -H "Content-Type: application/n-quads" \
      --repository "${STORE_REPOSITORY}-write" graph=${STORE_NAMED_GRAPH}-three <<EOF
 <http://example.com/default-subject> <http://example.com/default-predicate> "default object PUT2" .
 <http://example.com/named-subject> <http://example.com/named-predicate> "named object PUT2" <${STORE_NAMED_GRAPH}-two> .
@@ -26,7 +26,8 @@ curl_graph_store_get \
      -H "Accept: application/n-quads" --repository "${STORE_REPOSITORY}-write" \
    | tr -s '\n' '\t' \
    | fgrep '"default object"' | fgrep '"named object"' | fgrep  "<${STORE_NAMED_GRAPH}>" \
-   | fgrep -v "<${STORE_NAMED_GRAPH}-two>" | fgrep -v "PUT1" \
+   | fgrep -v "<${STORE_NAMED_GRAPH}-two>" \
+   | fgrep '"default object PUT1"' | fgrep '"named object PUT1"' \
    | fgrep '"default object PUT2"' | fgrep '"named object PUT2"' \
-   | tr -s '\t' '\n' | fgrep "${STORE_NAMED_GRAPH}-three" | wc -l | fgrep -q 2
+   | tr -s '\t' '\n' | fgrep "${STORE_NAMED_GRAPH}-three" | wc -l | fgrep -q 4
 

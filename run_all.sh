faeee9a330adcae8848d6f6c725972ca5a352981
james anderson <james@dydra.com>
2015-08-18 18:43:24 +0200

    [skip ci] add a toplevel runner

---
+++ run_all.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+date
+bash  ./run.sh extensions/sparql-protocol/temporal-data
+bash  ./run.sh extensions/sparql-protocol/collation
+bash  ./run.sh extensions/sparql-protocol/provenance
+bash  ./run.sh extensions/sparql-protocol/xpath-operators
+bash  ./run.sh extensions/sparql-protocol/values
+bash  ./run.sh extensions/sparql-protocol/meta-data
+bash  ./run.sh extensions/sparql-protocol/describe
+bash  ./run.sh sparql-protocol
+bash  ./run.sh sparql-graph-store-http-protocol

05c0c6f8dac75fae7dee97090334b2f8fe4771f3
james anderson <james@dydra.com>
2015-10-04 11:13:36 +0200

    [skip ci] add the sparql bnf to notes

---
+++ notes/README.md
@@ -0,0 +1,9 @@
+
+### Various Notes and Documents
+
+- rails_put_post : describes how to specify the "effective method" in POST requests
+
+- sparql-1-0-4.bnf : the current Dydra BNF for SPARQL request content.
+
+  This document is compiled via an atn compiler (https://github.com/lisp/de.setf.atn-parser) into the
+  parser at the core of Dydra's SPOCQ procesor.
---
+++ sparql-protocol/POST-srj.sh
@@ -0,0 +1,32 @@
+#! /bin/bash
+# check url encoding
+# check various graph parameters
+
+curl_sparql_request \
+     -H "Content-Type: application/x-www-form-urlencoded" \
+     -H "Accept: application/sparql-results+json" <<EOF \
+   | jq '.results.bindings[] | .[].value' | fgrep -q '"1"'
+query=select%20count(*)%20where%20%7b?s%20?p%20?o%7d
+EOF
+
+curl_sparql_request default-graph-uri=urn:dydra:default\
+     -H "Content-Type: application/sparql-query" \
+     -H "Accept: application/sparql-results+json" <<EOF \
+   | jq '.results.bindings[] | .[].value' | fgrep -q 'default'
+select * where { ?s ?p ?o }
+EOF
+
+
+curl_sparql_request  default-graph-uri=urn:dydra:all --repository plm\
+     -H "Content-Type: application/sparql-query" \
+     -H "Accept: application/sparql-results+json" <<EOF \
+   | cat # jq '.results.bindings[] | .[].value' | fgrep -q 'named'
+select * where { ?s ?p ?o }
+EOF
+
+curl_sparql_request  default-graph-uri=urn:dydra:named\
+     -H "Content-Type: application/sparql-query" \
+     -H "Accept: application/sparql-results+json" <<EOF \
+   | jq '.results.bindings[] | .[].value' | fgrep -q -v 'default'
+select * where { ?s ?p ?o }
+EOF

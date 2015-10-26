766018e5bf517bf5b83bde75a0a631cf7f29a721
james anderson <james@dydra.com>
2015-10-26 23:53:45 +0100

    [skip ci] extend tpf tests to verify metadata presence

--- triple-pattern-fragments/GET-----tpf.sh
+++ triple-pattern-fragments/GET-----tpf.sh
@@ -11,8 +11,8 @@ $CURL -f -s -X GET "${STORE_URL}/${STORE_ACCOUNT}/tpf/ldf?$subject&$predicate&$o
 
 fgrep -c example.org/subject result.nq | fgrep -q 12
 
-fgrep -c http://www.w3.org/ns/hydra/core#variable | fgrep -q 3
-fgrep -c http://www.w3.org/ns/hydra/core#property | fgrep -q 3
-fgrep -c http://www.w3.org/ns/hydra/core#mapping | fgrep -q 3
-fgrep -c http://www.w3.org/ns/hydra/core#template | fgrep -q 1
+fgrep -c http://www.w3.org/ns/hydra/core#variable result.nq | fgrep -q 3
+fgrep -c http://www.w3.org/ns/hydra/core#property result.nq | fgrep -q 3
+fgrep -c http://www.w3.org/ns/hydra/core#mapping result.nq | fgrep -q 3
+fgrep -c http://www.w3.org/ns/hydra/core#template result.nq | fgrep -q 1
 

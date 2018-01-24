4f541fcd9189c8e21a9d14361dbdb1c0bd1c6180
skorkmaz88 <skorkmaz88@yahoo.com>
2018-01-25 00:35:01 +0300

    Transcription control queries

---
+++ extensions/transcriptions/examples/transcribe_clear.sh
@@ -0,0 +1,4 @@
+curl -v -L -X POST -H "Accept: application/sparql-results+json" \
+ --data "TRANSCRIBE CLEAR" \
+-H "Content-Type: application/sparql-query" \
+'http://de8.dydra.com/skorkmaz/qa_test/dydra-query'

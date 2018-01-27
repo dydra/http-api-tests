620605f110b5cf85063bbbc7885d7df4b0bbbc28
skorkmaz88 <skorkmaz88@yahoo.com>
2018-01-27 22:17:21 +0300

    Transfer transcribe control to admin endpoint

--- extensions/transcriptions/examples/transcribe_list_verbose.sh
+++ extensions/transcriptions/examples/transcribe_list_verbose.sh
@@ -3,4 +3,4 @@
 curl -v -L -X POST -H "Accept: application/sparql-results+json" \
  --data "TRANSCRIBE LIST" \
 -H "Content-Type: application/sparql-query" \
-'http://de8.dydra.com/skorkmaz/qa_test/dydra-query'
+'http://de8.dydra.com/admin/transcribe'

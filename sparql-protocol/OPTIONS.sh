f7898d59def9022254ef6eedd224e4c3f109cf59
james anderson <james@dydra.com>
2018-03-29 23:51:20 +0200

    [skip ci] make options test ignore content type

--- sparql-protocol/OPTIONS.sh
+++ sparql-protocol/OPTIONS.sh
@@ -1,16 +1,13 @@
 #! /bin/bash
 set -o errexit
 
-# test without regards to excpusions based on content type
+# test without regards to exclusions based on content type
 # as the php implementation has no logic to distinguish
 # nb. -D - means no data
 
 # sparql query content combines with post only
 curl_sparql_request -D - -X OPTIONS\
      -H "Content-Type: application/sparql-query" \
-   | fgrep "Allow" | fgrep POST | fgrep -q HEAD
-# no content combines with get only
-curl_sparql_request -D - -X OPTIONS\
-     -H "Content-Type: " \
-   | fgrep "Allow" | fgrep GET | fgrep -q HEAD
+   | fgrep "Allow" | fgrep PUT | fgrep GET | fgrep POST | fgrep -q HEAD
+
 

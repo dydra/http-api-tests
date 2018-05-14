6b6718351f6f3944eef1d73d424c668995ec8c49
james anderson <james@dydra.com>
2018-05-14 04:24:15 +0200

    change auth token placement

--- triple-pattern-fragments/GET--p--tpf-revisions.sh
+++ triple-pattern-fragments/GET--p--tpf-revisions.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-revision=`${CURL} -s -H "Accept: text/plain" --user "${STORE_TOKEN}:" "${STORE_URL}/accounts/${STORE_ACCOUNT}/repositories/${STORE_REPOSITORY}/revisions" | tail -n 1`
+revision=`${CURL} -s -H "Accept: text/plain" --user ":${STORE_TOKEN}" "${STORE_URL}/accounts/${STORE_ACCOUNT}/repositories/${STORE_REPOSITORY}/revisions" | tail -n 1`
 
 curl_tpf_get "p=http%3A%2F%2Fexample.com%2Fdefault-predicate" --revision "${revision}" > result.nq
 fgrep -c default-subject result.nq | fgrep -q 1

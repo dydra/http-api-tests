3207c0078c262cf974820e75be1968e18fceb409
José R. Torregrosa Durán <jose@keepeyeonball.com>
2019-01-24 15:39:16 +0100

    no message

---
+++ dependencies_test.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+declare -A PACKBINMAP=( [jq]=jq [json_reformat]=yajl-tools [rapper]=raptor2-utils [tidy]=tidy)
+
+for BINARY in "${!PACKBINMAP[@]}"; do 
+	echo $BINARY "--->"  ${PACKBINMAP[$BINARY]}
+	which $BINARY > /dev/null; [ $? -eq 1 ] && echo "installing missing package ${PACKBINMAP[$BINARY]}" && sudo apt install -y ${PACKBINMAP[$BINARY]} ;
+done 
+

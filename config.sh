bd462429cea99c65427c4be0b1bea33263da1b3b
José R. Torregrosa Durán <jose@keepeyeonball.com>
2019-02-06 14:32:10 +0100

    sync between boxes, disregard

--- config.sh
+++ config.sh
@@ -1,4 +1,4 @@
-export STORE_URL="https://de8.dydra.com"
+export STORE_URL="https://test.dydra.com"
 export STORE_ACCOUNT="openrdf-sesame"
 export STORE_REPOSITORY="mem-rdf"
 export STORE_TOKEN="YtGwUWq1kY7nAL1ocIvl"

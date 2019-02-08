bd70d5c1e263791acefe59308a04fe9787130261
José R. Torregrosa Durán <jose@keepeyeonball.com>
2019-02-08 13:21:49 +0100

    one click config script

--- config.sh
+++ config.sh
@@ -1,6 +1,7 @@
+bash dependencies_test.sh
 export STORE_URL="https://test.dydra.com"
 export STORE_ACCOUNT="openrdf-sesame"
 export STORE_REPOSITORY="mem-rdf"
 export STORE_TOKEN="YtGwUWq1kY7nAL1ocIvl"
+export CURL="curl -kv"
 source define.sh
-bash run.sh extensions/sparql-protocol/temporal-data
--- jose-setup-scripts/new-thingy.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/bash
-
-curl_sparql_request \
-     -H "Accept: application/sparql-results+json" \
-     'query=select%20(count(*)%20as%20%3Fcount)%20where%20%7B%3Fs%20%3Fp%20%3Fo%7D' 

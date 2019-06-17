b5f6da68f69c0c011ab875c319dd12faa1fb7af9
james anderson <james@dydra.com>
2019-06-17 22:56:10 +0200

    further unsupported datatype tests

--- extensions/sparql-protocol/values/inline-update-datatypes.sh
+++ extensions/sparql-protocol/values/inline-update-datatypes.sh
@@ -19,7 +19,7 @@ where { { values (?value) { } }
       }
 EOF3
 ?value
-{  1 2.0 true 'a'@en  'b' <http://example.org> 'abcdef2'^^<http://example.org/datatype> 'P1Y2M'^^xsd:yearMonthDuration 'P1DT2H'^^xsd:dayTimeDuration '2014-01-02T10:11:12Z'^^xsd:dateTime '2014-01-01'^^xsd:date '10:11:12'^^xsd:time '2014'^^xsd:gYear }
+{  1 2.0 true 'a'@en  'b' <http://example.org> 'abcdefg'^^<http://example.org/datatype> 'P1Y2M'^^xsd:yearMonthDuration 'P1DT2H'^^xsd:dayTimeDuration '2014-01-02T10:11:12Z'^^xsd:dateTime '2014-01-01'^^xsd:date '10:11:12'^^xsd:time '2014'^^xsd:gYear }
 EOF4
 #EOF
 
@@ -37,7 +37,7 @@ cat > sort-precedence.txt <<EOF
 "2014-01-01"
 "10:11:12Z"
 "2014-01-02T10:11:12Z"
-"abcdef2"
+"abcdefg"
 EOF
 
 # see spocq:algepra/operators/basic-operators.lisp#type-sort-precedence

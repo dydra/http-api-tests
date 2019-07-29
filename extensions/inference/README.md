640dab300f5378c6bb7d788fe5c17c531f1c6eb6
james anderson <james@dydra.com>
2019-07-29 04:04:42 +0200

    [ci skip] add initial entailment test

--- extensions/inference/README.md
+++ extensions/inference/README.md
@@ -1,11 +1,43 @@
 # HTTP API tests : inference, abstraction, extension functions
 
-This directory comprises tests for three extensions
+This directory comprises tests SPARQL abstraction extensions.
+It is intended to cover the following mechanisms:
+
 - pattern-directed entailment :
-  a library of pattern BGPs is interpreted as abstract pattern entailments.
+  a library of pattern BGPs is interpreted as entailment rules.
 - extension functions :
   pattern predicates which bind functions are invoked with the bindings from the
   respective BGP nested-loop context and yield the results to the respective continuation.
 - view abstractions :
-  pattern predicates which designate views invoke those spaql expression as sub-queries.
+  pattern predicates which designate views invoke those SPARQL expression as sub-queries.
+
+
+## Pattern-Directed entailment
+
+The library's construct and n3 rules are are compiled into filter which activated
+BGP transformations. These tests cover
+
+- test different rule formats for simple rdfs entailment
+
+They should include also
+
+- the full set of standard rdfs entailment rules
+- published spin examples
+- a class subtyping use-case
+
+### rule formats
+
+rule-format.sh uses simplified SPIN, spin and N3 rules.
+
+### RDFS entailment
+
+### SPIN
+
+### class subtyping
+
+## Extension Functions
+
+## View Abstractions
+
+
 
---
+++ extensions/inference/rules-formats.sh
@@ -0,0 +1,72 @@
+#! /bin/bash
+#
+# test rdfs rules and their interpretation
+
+# set up the rules
+curl_graph_store_update -X PUT   -w "%{http_code}\n" -o /dev/null \
+     -H "Content-Type: text/turtle" \
+     --repository "inference"  <<EOF \
+   | test_put_success
+@prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> .
+@prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#> .
+<https://www.w3.org/TR/rdf11-mt/rdfs9#antecedant>
+    <http://www.w3.org/2000/10/swap/log/implies> <https://www.w3.org/TR/rdf11-mt/rdfs9#consequent> .
+
+<urn:dydra:entailment:rdfs2>
+    <http://spinrdf.org/spin#body> [
+        <http://spinrdf.org/sp#text> "construct {?subject a ?type} where {?predicate rdfs:domain ?type . ?subject ?predicate ?object}"^^<http://www.w3.org/2001/XMLSchema#string> ;
+        a <http://spinrdf.org/sp#Construct>
+    ] ;
+    a <http://spinrdf.org/spin#ConstructTemplate> .
+
+<urn:dydra:entailment:rdfs3>
+    <http://spinrdf.org/spin#body> [
+        <http://spinrdf.org/sp#text> "construct {?object a ?type} where {?predicate rdfs:range ?type . ?subject ?predicate ?object}"^^<http://www.w3.org/2001/XMLSchema#string> ;
+        a <http://spinrdf.org/sp#Construct>
+    ] ;
+    a <http://spinrdf.org/spin#ConstructTemplate> .
+
+<urn:dydra:entailment:rdfs7>
+    <http://spinrdf.org/sp#text> "construct {?subject ?predicate ?object} where {?subject ?subPredicate ?object . ?subPredicate rdfs:subPropertyOf ?predicate.}"^^<http://www.w3.org/2001/XMLSchema#string> ;
+    <http://www.w3.org/2000/01/rdf-schema#label> "rdfs7"^^<http://www.w3.org/2001/XMLSchema#string> .
+
+<urn:dydra:entailment:rdfs9>
+    <http://www.w3.org/2000/01/rdf-schema#label> "rdfs 9"^^<http://www.w3.org/2001/XMLSchema#string> ;
+    <http://www.w3.org/2000/10/swap/log/antecedent> <https://www.w3.org/TR/rdf11-mt/rdfs9#antecedant> ;
+    <http://www.w3.org/2000/10/swap/log/consequent> <https://www.w3.org/TR/rdf11-mt/rdfs9#consequent> .
+
+<urn:dydra:n3Rules>
+    rdf:_1 <urn:dydra:entailment:rdfs9> ;
+    a rdf:Bag .
+
+<urn:dydra:spTexts>
+    <http://spinrdf.org/spin#nextRuleProperty> <urn:dydra:n3Rules> ;
+    rdf:_1 <urn:dydra:entailment:rdfs7> ;
+    a rdf:Bag .
+
+<urn:dydra:spinRules>
+    <http://spinrdf.org/spin#nextRuleProperty> <urn:dydra:spTexts> ;
+    rdf:_1 <urn:dydra:entailment:rdfs2> ;
+    rdf:_2 <urn:dydra:entailment:rdfs3> ;
+    a rdf:Bag .
+
+[]
+    a _:w5eo568dz00cp5tv, [
+        <http://www.w3.org/2000/01/rdf-schema#subClassOf> _:w5eo568dz00cp5tv
+    ] .
+EOF
+
+# set up the data
+curl_graph_store_update -X PUT   -w "%{http_code}\n" -o /dev/null \
+     -H "Content-Type: application/n-triples" \
+     --repository "mem-rdf-write"  <<EOF \
+   | test_put_success
+<http://example.org/subject> <http://example.org/predicate> <http://example.org/object> .
+<http://example.org/predicate> <http://www.w3.org/2000/01/rdf-schema#domain> <http://example.org/class> .
+EOF
+
+# exercise the rules
+curl_sparql_request --repository "mem-rdf-write" -H "libraryPath: openrdf-sesamr/inference" <<EOF \
+ | jq '.results.bindings[] | .[].value' | fgrep -q "http://example.org/class"
+select * where {?s a ?class}
+EOF

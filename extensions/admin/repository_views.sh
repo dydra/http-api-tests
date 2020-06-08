3d5023f388d8d915a7449f481aa3b111dfb52874
james anderson <james@dydra.com>
2020-06-08 14:13:43 +0200

    [skip ci] add test for view metadata retrieval

---
+++ extensions/admin/repository_views.sh
@@ -0,0 +1,17 @@
+#! /bin/sh
+
+# test retrieving a repository's view list
+#
+
+# GET <repository>/views yields the view list
+$CURL -s -L -X GET -H "Accept: application/sparql-results+json" \
+  -u ":${AUTH_TOKEN}" \
+  "https://${STORE_HOST}/system/accounts/test/repositories/test/views" \
+ | fgrep -q "first-10"
+
+
+# GET <repository>/view/<view> yields the single view 
+$CURL -s -L -X GET -H "Accept: application/sparql-results+json" \
+  -u ":${AUTH_TOKEN}" \
+  "https://${STORE_HOST}/system/accounts/test/repositories/test/views/first-10" \
+ | fgrep -q "first-10"

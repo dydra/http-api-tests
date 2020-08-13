872b7f689eadb8a1a49ca434786d4b38c3d2da53
james anderson <james@dydra.com>
2020-08-13 17:18:19 +0200

    further spocq#307: test in variants [skip ci]

--- tickets/spocq-307.sh
+++ tickets/spocq-307.sh
@@ -11,10 +11,54 @@ curl_sparql_request \
 SELECT count(*) WHERE {
   VALUES (?this ?Type) {
     ( <https://localhost:4443/admin/acl/agents/e413f97b-15ee-47ea-ba65-4479aa7f1f9e/>
-       <https://localhost:4443/admin/ns#AgentItem> )
+      <https://localhost:4443/admin/ns#AgentItem> )
   }
   FILTER ((?this IN (<https://localhost:4443/admin/acl/agents/e413f97b-15ee-47ea-ba65-4479aa7f1f9e/>))
           )
            
 }
 EOF
+
+
+# this actually tests that the filter is not folded into the values clause
+
+curl_sparql_request \
+     -H "Accept: application/sparql-results+json" \
+     -H "Content-Type:application/sparql-query" <<EOF \
+ | tee $ECHO_OUTPUT | fgrep COUNT | fgrep -q '"1"'
+SELECT count(*) WHERE {
+    VALUES (?this ?Type) {
+    ( <https://localhost:4443/admin/acl/agents/e413f97b-15ee-47ea-ba65-4479aa7f1f9e/>
+      <https://localhost:4443/admin/ns#AgentItem> )
+ }
+ FILTER (?Type = <https://localhost:4443/admin/ns#AgentItem>)
+}
+EOF
+
+
+# test when it is folded into the bgp
+
+curl_sparql_request \
+     -H "Accept: application/sparql-results+json" \
+     -H "Content-Type:application/sparql-query" <<EOF \
+ | tee $ECHO_OUTPUT | fgrep COUNT | fgrep -q '"1"'
+SELECT count(*) WHERE {
+ ?s ?p ?o .
+ FILTER (?p IN (<http://example.com/default-predicate>))
+}
+EOF
+
+
+# test that unknown terms do not intefere with those which are known
+
+curl_sparql_request \
+     -H "Accept: application/sparql-results+json" \
+     -H "Content-Type:application/sparql-query" <<EOF \
+ | tee $ECHO_OUTPUT | fgrep COUNT | fgrep -q '"1"'
+SELECT count(*) WHERE {
+ ?s ?p ?o .
+ FILTER (?p IN (<http://example.com/default-predicate>,
+                <http://example.com/unknown-predicate>))
+}
+EOF
+

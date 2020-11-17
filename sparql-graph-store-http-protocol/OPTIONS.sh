80b015e986139276413268cffdf434ecdd59db5c
Max-Gerd Retzlaff <m.retzlaff@gmx.net>
2020-11-17 17:16:59 +0100

    update OPTIONS.sh tests: with http/2 field names are all lowercase

--- sparql-graph-store-http-protocol/OPTIONS.sh
+++ sparql-graph-store-http-protocol/OPTIONS.sh
@@ -7,14 +7,14 @@ set -o errexit
 
 # gsp
 curl_graph_store_get -D - -X OPTIONS \
-   | fgrep "Allow" | fgrep GET | fgrep -q DELETE
+   | fgrep -i "Allow" | fgrep GET | fgrep -q DELETE
 
 # there are no restrictions on request media type
 curl_graph_store_get -D - -f -s -X OPTIONS \
      -H "Content-Type: application/sparql-query" \
-   | fgrep "Allow" | fgrep GET | fgrep PUT | fgrep POST | fgrep -q DELETE
+   | fgrep -i "Allow" | fgrep GET | fgrep PUT | fgrep POST | fgrep -q DELETE
 
 # there are no restrictions on response media type
 curl_graph_store_get -D - -f -s -X OPTIONS \
      -H "Accept: application/sparql-results+xml" \
-   | fgrep "Allow" | fgrep PUT | fgrep POST | fgrep -q DELETE
+   | fgrep -i "Allow" | fgrep PUT | fgrep POST | fgrep -q DELETE
--- sparql-protocol/OPTIONS.sh
+++ sparql-protocol/OPTIONS.sh
@@ -8,6 +8,6 @@ set -o errexit
 # sparql query content combines with post only
 curl_sparql_request -D - -X OPTIONS\
      -H "Content-Type: application/sparql-query" \
-   | fgrep "Allow" | fgrep PUT | fgrep GET | fgrep POST | fgrep -q HEAD
+   | fgrep -i "Allow" | fgrep PUT | fgrep GET | fgrep POST | fgrep -q HEAD
 
 

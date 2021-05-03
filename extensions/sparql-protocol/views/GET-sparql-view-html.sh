601c611381cdd94eba604816177cb41377dffcbf
james anderson <james@dydra.com>
2021-05-03 14:27:59 +0200

    revise to use curl_sparql_view in order to get a view request url

--- extensions/sparql-protocol/views/GET-sparql-view-html.sh
+++ extensions/sparql-protocol/views/GET-sparql-view-html.sh
@@ -2,8 +2,8 @@
 
 # verify operation of an html view
 
-SPARQL_URL="${STORE_URL}/${STORE_ACCOUNT}/${STORE_REPOSITORY}/all.html" \
-curl_sparql_request  -X GET -H "Accept:" | egrep -q '<title>.*'${STORE_ACCOUNT}/${STORE_REPOSITORY}'.*</title>'
-
-#echo $SPARQL_URL
-#echo
+# SPARQL_URL="${STORE_URL}/${STORE_ACCOUNT}/${STORE_REPOSITORY}/all.html" \
+curl_sparql_view  -X GET \
+  -H "Accept:" \
+  -H "Content-Type:" all.html \
+  | egrep -q '<title>.*'${STORE_ACCOUNT}/${STORE_REPOSITORY}'.*</title>'

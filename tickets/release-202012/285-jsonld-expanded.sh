773c2433752a0c76b9e82f17ac7ab0c5c24b1cdb
james anderson <james@dydra.com>
2021-05-05 15:04:04 +0200

    json-ld encoding compact/expand corrections

--- tickets/release-202012/285-jsonld-expanded.sh
+++ tickets/release-202012/285-jsonld-expanded.sh
@@ -6,7 +6,7 @@
 curl_sparql_request \
      -H 'Accept: application/ld+json;profile=http://www.w3.org/ns/json-ld#expanded' \
      -H 'Content-Type:application/sparql-query' <<EOF \
- | tee $ECHO_OUTPUT | json_pp | json_diff /dev/stdin /dev/fd/3 3<<TEST 2>&1 | egrep -q '^\[\]$'
+ | tee $ECHO_OUTPUT | json_pp | json_diff /dev/stdin /dev/fd/3 3<<TEST 2>&1 | tee $ECHO_OUTPUT | egrep -q '^\[\]$'
 construct {
   [ <http://example.org#value> ?o ]
 }

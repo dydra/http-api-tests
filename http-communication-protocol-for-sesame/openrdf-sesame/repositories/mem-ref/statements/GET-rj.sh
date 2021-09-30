c8b0ce10376f190aec3d093c58e478db662272e1
james anderson <james@dydra.com>
2021-09-30 09:34:01 +0200

    allow sesame to respond with rdf+json

--- http-communication-protocol-for-sesame/openrdf-sesame/repositories/mem-ref/statements/GET-rj.sh
+++ http-communication-protocol-for-sesame/openrdf-sesame/repositories/mem-ref/statements/GET-rj.sh
@@ -4,6 +4,5 @@
 curl -w "%{http_code}\n"  -f -s -S -X GET\
      -H "Accept: application/rdf+json" \
      $STORE_URL/${STORE_ACCOUNT}/repositories/${STORE_REPOSITORY}/statements?auth_token=${STORE_TOKEN} \
-   | fgrep -q "${STATUS_NOT_ACCEPTABLE}"
+   | fgrep -q "${STATUS_OK}"
 
-echo -n " NYI "

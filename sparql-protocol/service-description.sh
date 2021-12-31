0354eab73be1427be09e1d852b4d5975f1748a09
james anderson <james@dydra.com>
2021-12-31 08:44:03 +0100

    [SKIP-CI] capture the service description for configuration information

--- sparql-protocol/service-description.sh
+++ sparql-protocol/service-description.sh
@@ -5,6 +5,7 @@
 
 curl_sparql_request -X GET -H "Accept: text/turtle" -H "Content-Type: " \
  | rapper -q --input turtle --output nquads /dev/stdin - \
+ | tee service-description.ttl \
  | fgrep -q 'http://www.w3.org/ns/sparql-service-description#Service'
 
 

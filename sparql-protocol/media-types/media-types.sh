f4aa761e41b774c3e9dd9e2a76973464a80b0e72
james anderson <james@dydra.com>
2021-12-31 08:45:08 +0100

    [SKIP-CI] distinguish json-ld compacted and expanded results; correct literal value syntax

--- sparql-protocol/media-types/media-types.sh
+++ sparql-protocol/media-types/media-types.sh
@@ -4,13 +4,19 @@
 set -e
 
 query="construct {?s ?p ?o} where { {graph ?g {?s ?p ?o}} union {?s ?p ?o} } order by ?s ?p ?o"
-function test_media_type() {
+function test_media_type () {
   local mime="$1";
-  local mime_file=`echo $mime | sed -e 's./._.' `;
+  mime_file=`echo $mime | sed -e 's./._.' `
+  test_media_type_with $mime $mime_file;
+}
+
+function test_media_type_with () {
+  local mime="$1";
+  local mime_file="$2";
   local ref_file="${mime_file}.ref"
   local new_file="${mime_file}.new"
 
-  echo "request: ${mime}" > $ECHO_OUTPUT
+  echo "request: ${mime}" "${new_file}" > $ECHO_OUTPUT
   curl_sparql_request -H "Accept: ${mime}" --repository mem-rdf-write <<EOF \
     | tee ${ECHO_OUTPUT} \
     | sed -e 's/.0E0"/.0"/' | sed -e 's/.0E0</.0</g'  | sed -e 's/.0E0,/.0,/g' > ${new_file}
@@ -39,12 +45,13 @@ curl_graph_store_update -X PUT -o /dev/null \
 
 test_media_type application/n-quads  sort
 test_media_type text/turtle
-
 # supported for import only
-# test_media_type application/trig "construct {?s ?p ?o} where { {graph ?g {?s ?p ?o}} union {?s ?p ?o} }"
-
+# test_media_type application/trig
 test_media_type application/trix
-test_media_type application/ld+json
+# specify compact or expand (default is a service configuration)
+#test_media_type application/ld+json 
+test_media_type_with 'application/ld+json;profile="http://www.w3.org/ns/json-ld#compacted"' application_ld+json_compacted
+test_media_type_with 'application/ld+json;profile="http://www.w3.org/ns/json-ld#expanded"' application_ld+json_expanded
 test_media_type application/rdf+xml
 test_media_type application/sparql-results+xml
 test_media_type application/sparql-results+json
@@ -55,18 +62,26 @@ test_media_type application/json
 
 # does not work as post.
 # requires a get to a view
-# test_media_type application/sparql-results+jsonp "select * where { {graph ?g {?s ?p ?o}} union {?s ?p ?o} }"
-# test_media_type application/javascript "select * where { {graph ?g {?s ?p ?o}} union {?s ?p ?o} }"
+# test_media_type application/sparql-results+jsonp
+# test_media_type application/javascript
 
-test_media_type text/tab-separated-values "select * where { {graph ?g {?s ?p ?o}} union {?s ?p ?o} }"
-test_media_type application/json "select * where { {graph ?g {?s ?p ?o}} union {?s ?p ?o} }"
+test_media_type text/tab-separated-values
+test_media_type application/json
 
 
 # for manual comparison
 cat > /dev/null <<EOF
-(test-sparql "select * where {graph ?g {?s ?p ?o}}"
+(test-sparql "select * where { {graph ?g {?s ?p ?o}} union {?s ?p ?o} } order by ?s ?p ?o"
              :repository-id "openrdf-sesame/mem-rdf-write"
-             :response-content-type mime:application/sparql-results+json)
+             ; :response-content-type mime:application/sparql-results+json
+)
+(test-sparql "construct {?s ?p ?o} where { {graph ?g {?s ?p ?o}} union {?s ?p ?o} } order by ?s ?p ?o"
+             :repository-id "openrdf-sesame/mem-rdf-write"
+             :response-content-type (mime-type "application/ld+json;profile=\"http://www.w3.org/ns/json-ld#compacted\"")
+             ; :response-content-type (mime-type "application/ld+json;profile=\"http://www.w3.org/ns/json-ld#expanded\"")
+             ; :response-content-type mime:application/ld+json
+             ;:response-content-type mime:text/turtle
+)
 EOF
 
 
@@ -113,6 +128,23 @@ where {
 order by ?s ?p ?o
 END
 
+curl_sparql_request -H 'Accept: application/ld+json;profile="http://www.w3.org/ns/json-ld#compacted"' --repository mem-rdf-write <<END
+construct { ?s ?p ?o}
+where {
+  {?s ?p ?o} 
+}
+order by ?s ?p ?o
+END
+
+curl_sparql_request -H 'Accept: application/ld+json;profile="http://www.w3.org/ns/json-ld#expanded"' --repository mem-rdf-write <<END
+construct { ?s ?p ?o}
+where {
+  {?s ?p ?o} 
+}
+order by ?s ?p ?o
+END
+
+
 dydra-import -i application/n-quads -X PUT openrdf-sesame/mem-rdf-write /dev/stdin <<END
 <http://example.org/default> <http://example.org/special> "string with comma (,)" .
 <http://example.org/default> <http://example.org/special> "string with comma (,)"^^<http://example.org/special> .
@@ -127,5 +159,14 @@ dydra-import -i application/n-quads -X PUT openrdf-sesame/mem-rdf-write /dev/std
 #<http://example.org/default> <http://www.w3.org/2001/XMLSchema#integer> "10"^^<http://www.w3.org/2001/XMLSchema#integer> .
 <http://example.org/default> <http://www.w3.org/2001/XMLSchema#integer> "0"^^<http://www.w3.org/2001/XMLSchema#integer> .
 END
+
+
+curl_graph_store_update -X PUT -o /dev/null \
+     -H "Content-Type: application/trig" \
+     --repository "${STORE_REPOSITORY_WRITABLE}" < media-types-int.trig
+
+curl_graph_store_update -X PUT -o /dev/null \
+     -H "Content-Type: application/trig" \
+     --repository "${STORE_REPOSITORY_WRITABLE}" < media-types-date.trig
 
 EOF

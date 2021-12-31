0a23e1fb16ba9e1221795eb14479ac05c2661baf
james anderson <james@dydra.com>
2021-12-31 08:46:04 +0100

    [SKIP-CI] allow the sequence numbers do not start at 1

--- sparql-protocol/GET-count-tsv.sh
+++ sparql-protocol/GET-count-tsv.sh
@@ -2,5 +2,5 @@
 
 curl_sparql_request -H "Accept: text/tab-separated-values" 'query=select%20count(*)%20where%20%7b?s%20?p%20?o%7d' \
    | tr -s '\n' '\t' \
-   | egrep -q -s 'COUNT1.*1'
+   | egrep -q -s 'COUNT.*1'
 
--- sparql-protocol/POST-count-srx.sh
+++ sparql-protocol/POST-count-srx.sh
@@ -6,8 +6,9 @@ curl_sparql_request \
      -H "Accept: application/sparql-results+xml" <<EOF \
    | xmllint  --c14n11 - \
    | tr -s '\t\n\r\f' ' ' | sed 's/ +/ /g' \
-   | fgrep -i 'variable name="count1"' \
-   | egrep -i -q -s '<binding name="count1">.*<literal .*>1</literal>'
+   | egrep -i 'variable name="count.*"' \
+   | tee ${ECHO_OUTPUT} \
+   | egrep -i -q -s '<binding name="count.*">.*<literal .*>1</literal>'
 query=select%20count(*)%20where%20%7b?s%20?p%20?o%7d
 EOF
 

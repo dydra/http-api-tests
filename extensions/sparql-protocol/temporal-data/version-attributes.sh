d893198d80e06581b061c1a4488de9c5578d3087
james anderson <james@dydra.com>
2022-03-17 00:57:11 +0100

    [SKIP-CI] resolve nxp#702: correct statement version constraints to make with and without versions attributes agree.

--- extensions/sparql-protocol/temporal-data/version-attributes.sh
+++ extensions/sparql-protocol/temporal-data/version-attributes.sh
@@ -92,13 +92,16 @@ where {
 EOF
 
 
-echo "next pass, with collated statements" > $ECHO_OUTPUT
+echo "next pass, with collated statements and PUT imports" > $ECHO_OUTPUT
 ### checks, also that it exists
 curl -s -X DELETE -H "Accept: text/turtle" --user ":${STORE_TOKEN}" -o $ECHO_OUTPUT \
   "https://${STORE_HOST}/system/accounts/test/repositories/test__rev/revisions"
 
 
 echo "create three revisions of collated statements" > $ECHO_OUTPUT
+# the dataset includes the statement in distinct revisions
+# the result is that HEAD or any specific revision sees just one set
+# and an explicit interval is required to see the content of multiple revisions.
 for i in 1 2 3; do
   curl_graph_store_update -X PUT -o $ECHO_OUTPUT \
      -H "Content-Type: text/turtle" \
@@ -110,7 +113,20 @@ for i in 1 2 3; do
 EOF
 done
 
-echo "next, verify version attributes of the second revision" > $ECHO_OUTPUT
+echo "next, verify the consitution of each revision"
+
+for revision in "HEAD" "HEAD~" "HEAD~2"; do
+  curl_sparql_request revision-id=${revision} \
+     --account test --repository test__rev <<EOF \
+     | tee $ECHO_OUTPUT | fgrep -c 'PUT' | fgrep -q "3"
+  select ?subject ?predicate ?object
+where {
+   ?subject ?predicate ?object
+}
+EOF
+done
+
+echo "next, verify version attributes of the second revision with the filter" > $ECHO_OUTPUT
 curl_sparql_request 'revision-id=*--*' \
    --account test --repository test__rev <<EOF \
    | tee $ECHO_OUTPUT | fgrep -c 'default-subject' | fgrep -q "9"
@@ -120,7 +136,7 @@ prefix time: <http://www.w3.org/2006/time#>
 prefix : <http://example.org#>
 
 select ?subject ?predicate ?object
-       ?predecessorDeletedOrdinal ?addedOrdinal ?deletedOrdinal ?successorAddedOrdinal
+       ?predecessorDeletedOrdinal ?addedOrdinal ?deletedOrdinal ?successorAddedOrdinal ( dydraOp:repository-end-ordinal() as ?eo)
 where {
    # dydra:met-by is the end of previous visibility
    # dydra:starts is the current visibility with modified content
@@ -134,3 +150,93 @@ where {
    filter (?deletedOrdinal = dydraOp:repository-end-ordinal())
 }
 EOF
+
+### j-walker additions
+
+echo "next pass, with POST imports which yield incrementally inserted statements" > $ECHO_OUTPUT
+### checks, also that it exists
+curl -s -X DELETE -H "Accept: text/turtle" --user ":${STORE_TOKEN}" -o $ECHO_OUTPUT \
+  "https://${STORE_HOST}/system/accounts/test/repositories/test__rev/revisions"
+
+echo "create three revisions" > $ECHO_OUTPUT
+for i in 1 2 3; do
+  curl_graph_store_update -X POST -o $ECHO_OUTPUT \
+     -H "Content-Type: text/turtle" \
+     --account test --repository test__rev default <<EOF
+<http://example.com/default-subject>
+    <http://example.com/default-predicate> "default object POST${i}" .
+EOF
+done
+
+echo "next, verify revisions with the correct number of statements" > $ECHO_OUTPUT
+curl_sparql_request revision-id=HEAD \
+   --account test --repository test__rev <<EOF \
+   | tee $ECHO_OUTPUT | fgrep -c 'POST' | fgrep -q "3"
+select ?subject ?predicate ?object
+where {
+   ?subject ?predicate ?object
+}
+EOF
+curl_sparql_request revision-id=HEAD~ \
+   --account test --repository test__rev <<EOF \
+   | tee $ECHO_OUTPUT | fgrep -c 'POST' | fgrep -q "2"
+select ?subject ?predicate ?object
+where {
+   ?subject ?predicate ?object
+}
+EOF
+curl_sparql_request revision-id=HEAD~2 \
+   --account test --repository test__rev <<EOF \
+   | tee $ECHO_OUTPUT | fgrep -c 'POST' | fgrep -q "1"
+select ?subject ?predicate ?object
+where {
+   ?subject ?predicate ?object
+}
+EOF
+
+echo "next, verify annotated statement pattern also returns the correct number of statements" > $ECHO_OUTPUT
+curl_sparql_request revision-id=HEAD \
+   --account test --repository test__rev <<EOF \
+   | tee $ECHO_OUTPUT | fgrep -c 'POST' | fgrep -q "3"
+prefix dydra: <urn:dydra>
+select ?subject ?predicate ?object ?addedOrdinal
+where {
+   ?subject ?predicate ?object {| dydra:starts ?addedOrdinal |}
+}
+EOF
+curl_sparql_request revision-id=HEAD~ \
+   --account test --repository test__rev <<EOF \
+   | tee $ECHO_OUTPUT | fgrep -c 'POST' | fgrep -q "2"
+prefix dydra: <urn:dydra>
+select ?subject ?predicate ?object ?addedOrdinal
+where {
+   ?subject ?predicate ?object {| dydra:starts ?addedOrdinal |}
+}
+EOF
+curl_sparql_request revision-id=HEAD~2 \
+   --account test --repository test__rev <<EOF \
+   | tee $ECHO_OUTPUT | fgrep -c 'POST' | fgrep -q "1"
+prefix dydra: <urn:dydra>
+select ?subject ?predicate ?object ?addedOrdinal
+where {
+   ?subject ?predicate ?object {| dydra:starts ?addedOrdinal |}
+}
+EOF
+
+echo "next, verify that three annotated statement pattern are visible for *--*" > $ECHO_OUTPUT
+curl_sparql_request revision-id='*--*' \
+   --account test --repository test__rev <<EOF \
+   | tee $ECHO_OUTPUT | fgrep -c 'POST' | fgrep -q "3"
+prefix dydra: <urn:dydra>
+select ?subject ?predicate ?object ?addedOrdinal
+where {
+   ?subject ?predicate ?object {| dydra:starts ?addedOrdinal |}
+}
+EOF
+
+
+
+
+
+
+

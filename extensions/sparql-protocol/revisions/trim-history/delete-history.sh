2bc1a70decf9dac64383812b8d1e1281f6f8b0be
Max-Gerd Retzlaff <mgr@matroid.org>
2023-02-09 19:27:02 +0100

    improve trim-revision tests to check all revision contents

--- extensions/sparql-protocol/revisions/trim-history/delete-history.sh
+++ extensions/sparql-protocol/revisions/trim-history/delete-history.sh
@@ -8,9 +8,10 @@ then
   export INFO_OUTPUT=${ECHO_OUTPUT} # /dev/null # /dev/tty
 fi
 
-# q option for grep
-q=-q
-#q=""
+if [[ "" == "${GREP_OUTPUT:-}" ]]
+then
+  export GREP_OUTPUT=${ECHO_OUTPUT} # /dev/null # /dev/tty
+fi
 
 if ( repository_is_revisioned --repository ${repository})
 then
@@ -42,24 +43,45 @@ repository_number_of_revisions --repository ${repository} | fgrep -qx "1"
 
 add_quad 1
 repository_number_of_revisions --repository ${repository} | fgrep -qx "2"
-curl_graph_store_get --repository mem-rdf-revisioned | tr -s '\n' '\t' | fgrep $q "object-1"
+curl_graph_store_get --repository mem-rdf-revisioned | tr -s '\n' '\t' | fgrep "object-1" > ${GREP_OUTPUT}
 
 add_quad 2
 repository_number_of_revisions --repository ${repository} | fgrep -qx "3"
-curl_graph_store_get --repository mem-rdf-revisioned | tr -s '\n' '\t' | fgrep "object-1" | fgrep $q "object-2"
+curl_graph_store_get --repository mem-rdf-revisioned | tr -s '\n' '\t' | fgrep "object-1" | fgrep "object-2" > ${GREP_OUTPUT}
 
 add_quad 3
 repository_number_of_revisions --repository ${repository} | fgrep -qx "4"
-curl_graph_store_get --repository mem-rdf-revisioned | tr -s '\n' '\t' | fgrep "object-1" | fgrep "object-2" | fgrep $q "object-3"
+curl_graph_store_get --repository mem-rdf-revisioned | tr -s '\n' '\t' | fgrep "object-1" | fgrep "object-2" | fgrep "object-3" > ${GREP_OUTPUT}
 
-repository_list_revisions --repository ${repository}
 
-echo "delete revisions again and test" > ${INFO_OUTPUT}
+echo "check visibilities of quads in last three revisions" > ${INFO_OUTPUT}
+curl_graph_store_get --repository mem-rdf-revisioned revision-id=HEAD~2 | tr -s '\n' '\t' | fgrep "object-1" | fgrep -v "object-2" | fgrep -v "object-3" > ${GREP_OUTPUT}
+curl_graph_store_get --repository mem-rdf-revisioned revision-id=HEAD~1 | tr -s '\n' '\t' | fgrep "object-1" | fgrep "object-2" | fgrep -v "object-3" > ${GREP_OUTPUT}
+curl_graph_store_get --repository mem-rdf-revisioned revision-id=HEAD   | tr -s '\n' '\t' | fgrep "object-1" | fgrep "object-2" | fgrep "object-3" > ${GREP_OUTPUT}
+
+#repository_list_revisions --repository ${repository}
+
+echo "calling DELETE-HISTORY: remove all revisions prior to HEAD~1" > ${INFO_OUTPUT}
 delete_revisions --repository ${repository} revision-id=HEAD~1 mode=delete-history
 #delete_revisions --repository ${repository} revision-id=HEAD~1 mode=project-history
-repository_number_of_revisions --repository ${repository} | fgrep -qx "3" # still 3 as delete-history adds one revision itself
+repository_number_of_revisions --repository ${repository} | fgrep -qx "3"
+echo "have three revisions now: HEAD~1, HEAD, and a new revision from the trim-history command itself" > ${INFO_OUTPUT}
 
-repository_list_revisions --repository ${repository}
+#repository_list_revisions --repository ${repository}
+
+
+
+echo "check state after trim-history now" > ${INFO_OUTPUT}
+## this part is different in delete-history and project-history
 
 # object-1 is deleted, all other still present:
-curl_graph_store_get --repository mem-rdf-revisioned | tr -s '\n' '\t' | fgrep -v "object-1" | fgrep "object-2" | fgrep $q "object-3"
+curl_graph_store_get --repository mem-rdf-revisioned | tr -s '\n' '\t' | fgrep -v "object-1" | fgrep "object-2" | fgrep "object-3" > ${GREP_OUTPUT}
+
+echo "check visibilities of quads in last three revisions again" > ${INFO_OUTPUT}
+# the old revision HEAD~2 which introduced object-1 is completely deleted, so object-1 is not present anywhere.
+# HEAD~2 just introduces object-2
+curl_graph_store_get --repository mem-rdf-revisioned revision-id=HEAD~2 | tr -s '\n' '\t' | fgrep -v "object-1" | fgrep "object-2" | fgrep -v "object-3" > ${GREP_OUTPUT}
+# HEAD~1 is the old HEAD, minus object-1
+curl_graph_store_get --repository mem-rdf-revisioned revision-id=HEAD~1 | tr -s '\n' '\t' | fgrep -v "object-1" | fgrep "object-2" | fgrep "object-3" > ${GREP_OUTPUT}
+# HEAD is just identical to HEAD~1
+curl_graph_store_get --repository mem-rdf-revisioned revision-id=HEAD   | tr -s '\n' '\t' | fgrep -v "object-1" | fgrep "object-2" | fgrep "object-3" > ${GREP_OUTPUT}
--- extensions/sparql-protocol/revisions/trim-history/project-revisions.sh
+++ extensions/sparql-protocol/revisions/trim-history/project-revisions.sh
@@ -55,9 +55,9 @@ curl_graph_store_get --repository mem-rdf-revisioned | tr -s '\n' '\t' | fgrep "
 
 
 echo "check visibilities of quads in last three revisions" > ${INFO_OUTPUT}
-curl_graph_store_get --repository mem-rdf-revisioned revision-id=HEAD~2 | tr -s '\n' '\t' | fgrep "object-1" > ${GREP_OUTPUT}
-curl_graph_store_get --repository mem-rdf-revisioned revision-id=HEAD~1 | tr -s '\n' '\t' | fgrep "object-1" | fgrep "object-2" > ${GREP_OUTPUT}
-curl_graph_store_get --repository mem-rdf-revisioned revision-id=HEAD | tr -s '\n' '\t' | fgrep "object-1" | fgrep "object-2" | fgrep "object-3" > ${GREP_OUTPUT}
+curl_graph_store_get --repository mem-rdf-revisioned revision-id=HEAD~2 | tr -s '\n' '\t' | fgrep "object-1" | fgrep -v "object-2" | fgrep -v "object-3" > ${GREP_OUTPUT}
+curl_graph_store_get --repository mem-rdf-revisioned revision-id=HEAD~1 | tr -s '\n' '\t' | fgrep "object-1" | fgrep "object-2" | fgrep -v "object-3" > ${GREP_OUTPUT}
+curl_graph_store_get --repository mem-rdf-revisioned revision-id=HEAD   | tr -s '\n' '\t' | fgrep "object-1" | fgrep "object-2" | fgrep "object-3" > ${GREP_OUTPUT}
 
 #repository_list_revisions --repository ${repository}
 
@@ -69,10 +69,18 @@ echo "have three revisions now: HEAD~1, HEAD, and a new revision from the trim-h
 
 #repository_list_revisions --repository ${repository}
 
+
+
+echo "check state after trim-history now" > ${INFO_OUTPUT}
+## this part is different in delete-history and project-history
+
 # object-1 is still present, all other also present:
 curl_graph_store_get --repository mem-rdf-revisioned | tr -s '\n' '\t' | fgrep "object-1" | fgrep "object-2" | fgrep "object-3" > ${GREP_OUTPUT}
 
 echo "check visibilities of quads in last three revisions again" > ${INFO_OUTPUT}
-curl_graph_store_get --repository mem-rdf-revisioned revision-id=HEAD~2 | tr -s '\n' '\t' | fgrep "object-1" | fgrep "object-2" > ${GREP_OUTPUT}
-curl_graph_store_get --repository mem-rdf-revisioned revision-id=HEAD~1 | tr -s '\n' '\t' | fgrep "object-1" | fgrep "object-2" > ${GREP_OUTPUT}
-curl_graph_store_get --repository mem-rdf-revisioned revision-id=HEAD | tr -s '\n' '\t' | fgrep "object-1" | fgrep "object-2" | fgrep "object-3" > ${GREP_OUTPUT}
+# HEAD~2 now introduces also object-1 in addition to object-2 -> phantom insert
+curl_graph_store_get --repository mem-rdf-revisioned revision-id=HEAD~2 | tr -s '\n' '\t' | fgrep "object-1" | fgrep "object-2" | fgrep -v "object-3" > ${GREP_OUTPUT}
+# HEAD~1 is the old HEAD
+curl_graph_store_get --repository mem-rdf-revisioned revision-id=HEAD~1 | tr -s '\n' '\t' | fgrep "object-1" | fgrep "object-2" | fgrep "object-3" > ${GREP_OUTPUT}
+# HEAD is just identical to HEAD~1
+curl_graph_store_get --repository mem-rdf-revisioned revision-id=HEAD   | tr -s '\n' '\t' | fgrep "object-1" | fgrep "object-2" | fgrep "object-3" > ${GREP_OUTPUT}

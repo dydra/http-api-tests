ccc2566f6bdd6bd19604061e527c048b788a8067
Max-Gerd Retzlaff <mgr@matroid.org>
2023-02-10 17:18:34 +0100

    unify delete-revisions.sh with other tests

--- extensions/admin/revisions/delete-revisions.sh
+++ extensions/admin/revisions/delete-revisions.sh
@@ -13,18 +13,28 @@ else
   exit 1
 fi
 
-echo "initial test after delete revisions" > ${ECHO_OUTPUT}
-delete_revisions --repository ${repository} | fgrep -xq 200
-repository_number_of_revisions --repository ${repository} | fgrep -qx "1"
+if [[ "" == "${INFO_OUTPUT:-}" ]]
+then
+  export INFO_OUTPUT=${ECHO_OUTPUT} # /dev/null # /dev/tty
+fi
 
-echo "put something in, thus creating a second revision" > ${ECHO_OUTPUT}
+if [[ "" == "${GREP_OUTPUT:-}" ]]
+then
+  export GREP_OUTPUT=${ECHO_OUTPUT} # /dev/null # /dev/tty
+fi
+
+echo "initial test after delete revisions" > ${INFO_OUTPUT}
+delete_revisions --repository ${repository} | fgrep -x 200 > ${GREP_OUTPUT}
+repository_number_of_revisions --repository ${repository} | fgrep -x "1" > ${GREP_OUTPUT}
+
+echo "put something in, thus creating a second revision" > ${INFO_OUTPUT}
 curl_graph_store_update --repository ${repository} -X PUT -o /dev/null <<EOF \
  | tee ${ECHO_OUTPUT}
 <http://example.com/default-subject> <http://example.com/default-predicate> "default object" .
 EOF
 
-repository_number_of_revisions --repository ${repository} | fgrep -qx "2"
+repository_number_of_revisions --repository ${repository} | fgrep -x "2" > ${GREP_OUTPUT}
 
-echo "delete revisions again and test" > ${ECHO_OUTPUT}
-delete_revisions --repository ${repository} | fgrep -xq 200
-repository_number_of_revisions --repository ${repository} | fgrep -qx "1"
+echo "delete revisions again and test" > ${INFO_OUTPUT}
+delete_revisions --repository ${repository} | fgrep -x 200 > ${GREP_OUTPUT}
+repository_number_of_revisions --repository ${repository} | fgrep -x "1" > ${GREP_OUTPUT}

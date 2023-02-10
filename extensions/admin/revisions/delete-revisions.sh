2b3b4db4eee50f4feb52ab6ad098b8f40c019122
Max-Gerd Retzlaff <mgr@matroid.org>
2023-02-10 17:47:45 +0100

    add new test trim-revisions/HEAD.sh and simplify delete-revisions.sh test

--- extensions/admin/revisions/delete-revisions.sh
+++ extensions/admin/revisions/delete-revisions.sh
@@ -6,13 +6,9 @@ echo "initial test after delete revisions" > ${INFO_OUTPUT}
 delete_revisions --repository ${repository} | fgrep -x 200 > ${GREP_OUTPUT}
 repository_number_of_revisions --repository ${repository} | fgrep -x "1" > ${GREP_OUTPUT}
 
-echo "put something in, thus creating a second revision" > ${INFO_OUTPUT}
-curl_graph_store_update --repository ${repository} -X PUT -o /dev/null <<EOF \
- | tee ${ECHO_OUTPUT}
-<http://example.com/default-subject> <http://example.com/default-predicate> "default object" .
-EOF
-
-repository_number_of_revisions --repository ${repository} | fgrep -x "2" > ${GREP_OUTPUT}
+add_quad foo
+repository_number_of_revisions --repository ${repository} | fgrep -qx "2"
+curl_graph_store_get --repository mem-rdf-revisioned | tr -s '\n' '\t' | fgrep "object-foo" > ${GREP_OUTPUT}
 
 echo "delete revisions again and test" > ${INFO_OUTPUT}
 delete_revisions --repository ${repository} | fgrep -x 200 > ${GREP_OUTPUT}
---
+++ extensions/admin/revisions/trim-history/HEAD.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+
+source ../init-revisions-tests.sh
+
+echo "initial test after delete revisions" > ${INFO_OUTPUT}
+delete_revisions --repository ${repository} | fgrep -x 200 > ${GREP_OUTPUT}
+repository_number_of_revisions --repository ${repository} | fgrep -x "1" > ${GREP_OUTPUT}
+
+add_quad 1
+repository_number_of_revisions --repository ${repository} | fgrep -qx "2"
+curl_graph_store_get --repository mem-rdf-revisioned | tr -s '\n' '\t' | fgrep "object-1" > ${GREP_OUTPUT}
+
+add_quad 2
+repository_number_of_revisions --repository ${repository} | fgrep -qx "3"
+curl_graph_store_get --repository mem-rdf-revisioned | tr -s '\n' '\t' | fgrep "object-1" | fgrep "object-2" > ${GREP_OUTPUT}
+
+echo "remove all revisions prior to HEAD should leave just one revision + the operations revision" > ${INFO_OUTPUT}
+delete_revisions --repository ${repository} revision-id=HEAD mode=delete-history | fgrep -x 200 > ${GREP_OUTPUT}
+repository_number_of_revisions --repository ${repository} | fgrep -x "2" > ${GREP_OUTPUT}

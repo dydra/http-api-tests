69e96fd47922e5be7e2c964a2cc7272e47ee174e
Max-Gerd Retzlaff <mgr@matroid.org>
2023-02-17 19:21:46 +0100

    trim revisions: improve block1.1 test

--- extensions/admin/revisions/trim-history/block1.1-tests-TAIL.sh
+++ extensions/admin/revisions/trim-history/block1.1-tests-TAIL.sh
@@ -17,15 +17,18 @@ test "$result" -eq "404"
 
 add_quad 1
 repository_number_of_revisions --repository ${repository} | fgrep -x "2" > ${GREP_OUTPUT}
-curl_graph_store_get --repository ${repository} | tr -s '\n' '\t' | fgrep "object-1" > ${GREP_OUTPUT}
+curl_graph_store_get --repository ${repository} | tr -s '\n' '\t' \
+    | fgrep "object-1" | fgrep -v "object-2" > ${GREP_OUTPUT}
 
 add_quad 2
 repository_number_of_revisions --repository ${repository} | fgrep -x "3" > ${GREP_OUTPUT}
-curl_graph_store_get --repository ${repository} | tr -s '\n' '\t' | fgrep "object-1" | fgrep "object-2" > ${GREP_OUTPUT}
+curl_graph_store_get --repository ${repository} | tr -s '\n' '\t' \
+    | fgrep "object-1" | fgrep    "object-2" > ${GREP_OUTPUT}
 
 echo "checking visibility vectors before trim-history" > ${INFO_OUTPUT}
 get_visibility | tr -s '\n' '\t' | tee ${INFO_OUTPUT} \
     | fgrep '"object-1,2"' | fgrep '"object-2,3"' > ${GREP_OUTPUT}
+
 
 echo "remove all revisions prior to TAIL should do nothing as TAIL has no history (in mode \"${mode}\")" > ${INFO_OUTPUT}
 # Note the test for HTTP 204 here, as delete-history of TAIL should lead to 204 no content:

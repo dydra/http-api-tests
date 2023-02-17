a3d615fe8d05d1eb9978169671ed9cfd3986615c
Max-Gerd Retzlaff <mgr@matroid.org>
2023-02-17 19:37:10 +0100

    trim-revision: add visibility vector test to delete-revisions/history and project-history

--- extensions/admin/revisions/delete-revisions.sh
+++ extensions/admin/revisions/delete-revisions.sh
@@ -8,8 +8,18 @@ repository_number_of_revisions --repository ${repository} | fgrep -x "1" > ${GRE
 
 add_quad foo
 repository_number_of_revisions --repository ${repository} | fgrep -x "2" > ${GREP_OUTPUT}
-curl_graph_store_get --repository ${repository} | tr -s '\n' '\t' | fgrep "object-foo" > ${GREP_OUTPUT}
+curl_graph_store_get --repository ${repository} | tr -s '\n' '\t' \
+    | fgrep "object-foo" > ${GREP_OUTPUT}
+
+echo "checking visibility vectors before trim-history" > ${INFO_OUTPUT}
+get_visibility | tr -s '\n' '\t' | tee ${INFO_OUTPUT} \
+    | fgrep '"object-foo,2"' > ${GREP_OUTPUT}
+
 
 echo "delete revisions again and test" > ${INFO_OUTPUT}
 delete_revisions --repository ${repository} | fgrep -x 200 > ${GREP_OUTPUT}
 repository_number_of_revisions --repository ${repository} | fgrep -x "1" > ${GREP_OUTPUT}
+
+echo "checking visibility vectors after delete-revisions" > ${INFO_OUTPUT}
+statements=$(get_visibility | tr -s '\n' '\t' | tee ${INFO_OUTPUT} | wc -l)
+test "$statements" -eq "0"
--- extensions/admin/revisions/project-history.sh
+++ extensions/admin/revisions/project-history.sh
@@ -8,21 +8,34 @@ repository_number_of_revisions --repository ${repository} | fgrep -x "1" > ${GRE
 
 add_quad 1
 repository_number_of_revisions --repository ${repository} | fgrep -x "2" > ${GREP_OUTPUT}
-curl_graph_store_get --repository ${repository} | tr -s '\n' '\t' | fgrep "object-1" > ${GREP_OUTPUT}
+curl_graph_store_get --repository ${repository} | tr -s '\n' '\t' \
+    | fgrep "object-1" | fgrep -v "object-2" | fgrep -v "object-3" > ${GREP_OUTPUT}
 
 add_quad 2
 repository_number_of_revisions --repository ${repository} | fgrep -x "3" > ${GREP_OUTPUT}
-curl_graph_store_get --repository ${repository} | tr -s '\n' '\t' | fgrep "object-1" | fgrep "object-2" > ${GREP_OUTPUT}
+curl_graph_store_get --repository ${repository} | tr -s '\n' '\t' \
+    | fgrep "object-1" | fgrep    "object-2" | fgrep -v "object-3" > ${GREP_OUTPUT}
 
 add_quad 3
 repository_number_of_revisions --repository ${repository} | fgrep -x "4" > ${GREP_OUTPUT}
-curl_graph_store_get --repository ${repository} | tr -s '\n' '\t' | fgrep "object-1" | fgrep "object-2" | fgrep "object-3" > ${GREP_OUTPUT}
+curl_graph_store_get --repository ${repository} | tr -s '\n' '\t' \
+    | fgrep "object-1" | fgrep    "object-2" | fgrep    "object-3" > ${GREP_OUTPUT}
 
 
 echo "check visibilities of quads in last three revisions" > ${INFO_OUTPUT}
-curl_graph_store_get --repository ${repository} revision-id=HEAD~2 | tr -s '\n' '\t' | fgrep "object-1" | fgrep -v "object-2" | fgrep -v "object-3" > ${GREP_OUTPUT}
-curl_graph_store_get --repository ${repository} revision-id=HEAD~1 | tr -s '\n' '\t' | fgrep "object-1" | fgrep "object-2" | fgrep -v "object-3" > ${GREP_OUTPUT}
-curl_graph_store_get --repository ${repository} revision-id=HEAD   | tr -s '\n' '\t' | fgrep "object-1" | fgrep "object-2" | fgrep "object-3" > ${GREP_OUTPUT}
+curl_graph_store_get --repository ${repository} revision-id=HEAD~2 | tr -s '\n' '\t' \
+    | fgrep "object-1" | fgrep -v "object-2" | fgrep -v "object-3" > ${GREP_OUTPUT}
+curl_graph_store_get --repository ${repository} revision-id=HEAD~1 | tr -s '\n' '\t' \
+    | fgrep "object-1" | fgrep    "object-2" | fgrep -v "object-3" > ${GREP_OUTPUT}
+curl_graph_store_get --repository ${repository} revision-id=HEAD   | tr -s '\n' '\t' \
+    | fgrep "object-1" | fgrep    "object-2" | fgrep    "object-3" > ${GREP_OUTPUT}
+
+echo "checking visibility vectors before trim-history" > ${INFO_OUTPUT}
+get_visibility | tr -s '\n' '\t' | tee ${INFO_OUTPUT} \
+    | fgrep    '"object-1,2"' \
+    | fgrep    '"object-2,3"' \
+    | fgrep    '"object-3,4"' > ${GREP_OUTPUT}
+
 
 #repository_list_revisions --repository ${repository}
 
@@ -40,12 +53,23 @@ echo "check state after trim-history now" > ${INFO_OUTPUT}
 ## this part is different in delete-history and project-history
 
 # object-1 is still present, all other also present:
-curl_graph_store_get --repository ${repository} | tr -s '\n' '\t' | fgrep "object-1" | fgrep "object-2" | fgrep "object-3" > ${GREP_OUTPUT}
+curl_graph_store_get --repository ${repository} | tr -s '\n' '\t' \
+    | fgrep "object-1" | fgrep "object-2" | fgrep "object-3" > ${GREP_OUTPUT}
 
 echo "check visibilities of quads in last three revisions again" > ${INFO_OUTPUT}
 # HEAD~2 now introduces also object-1 in addition to object-2 -> phantom insert
-curl_graph_store_get --repository ${repository} revision-id=HEAD~2 | tr -s '\n' '\t' | fgrep "object-1" | fgrep "object-2" | fgrep -v "object-3" > ${GREP_OUTPUT}
+curl_graph_store_get --repository ${repository} revision-id=HEAD~2 | tr -s '\n' '\t' \
+    | fgrep "object-1" | fgrep "object-2" | fgrep -v "object-3" > ${GREP_OUTPUT}
 # HEAD~1 is the old HEAD
-curl_graph_store_get --repository ${repository} revision-id=HEAD~1 | tr -s '\n' '\t' | fgrep "object-1" | fgrep "object-2" | fgrep "object-3" > ${GREP_OUTPUT}
+curl_graph_store_get --repository ${repository} revision-id=HEAD~1 | tr -s '\n' '\t' \
+    | fgrep "object-1" | fgrep "object-2" | fgrep    "object-3" > ${GREP_OUTPUT}
 # HEAD is just identical to HEAD~1
-curl_graph_store_get --repository ${repository} revision-id=HEAD   | tr -s '\n' '\t' | fgrep "object-1" | fgrep "object-2" | fgrep "object-3" > ${GREP_OUTPUT}
+curl_graph_store_get --repository ${repository} revision-id=HEAD   | tr -s '\n' '\t' \
+    | fgrep "object-1" | fgrep "object-2" | fgrep    "object-3" > ${GREP_OUTPUT}
+
+
+echo "checking visibility vectors after trim-history in mode \"project-history\"" > ${INFO_OUTPUT}
+get_visibility | tr -s '\n' '\t' | tee ${INFO_OUTPUT} \
+    | fgrep    '"object-1,3"' \
+    | fgrep    '"object-2,3"' \
+    | fgrep    '"object-3,4"' > ${GREP_OUTPUT}

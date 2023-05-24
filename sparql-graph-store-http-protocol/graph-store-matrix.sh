ebfa8701ea1e925f8391452b3a37c62ce41ada5c
Max-Gerd Retzlaff <mgr@matroid.org>
2023-05-24 11:03:55 +0200

    add 3rd version of graph-store-matrix.sh was named matrix.sh_bck3_before-double-response

--- sparql-graph-store-http-protocol/graph-store-matrix.sh
+++ sparql-graph-store-http-protocol/graph-store-matrix.sh
@@ -1,18 +1,24 @@
 #!/bin/bash
 protocolgraph="<${STORE_NAMED_GRAPH}-protocol>"
+read -d '' initial_input <<EOF
+<http://example.com/default-subject> <http://example.com/default-predicate> "default-object-org" .
+<http://example.com/named-subject> <http://example.com/named-predicate> "named-object-GRAPH1-org" <${STORE_NAMED_GRAPH}-one> .
+<http://example.com/named-subject> <http://example.com/named-predicate> "named-object-GRAPH2-org" <${STORE_NAMED_GRAPH}-two> .
+<http://example.com/named-subject> <http://example.com/named-predicate> "named-object-GRAPH3-org" ${protocolgraph} .
+EOF
 read -d '' input_triples <<EOF
 <http://example.com/default-subject> <http://example.com/default-predicate> "default-object-new" .
 EOF
 read -d '' input_quads <<EOF
 <http://example.com/default-subject> <http://example.com/default-predicate> "default-object-new" .
-<http://example.com/named-subject> <http://example.com/named-predicate> "named-object-GRAPH1-new" <${STORE_NAMED_GRAPH}> .
-<http://example.com/named-subject> <http://example.com/named-predicate> "named-object-GRAPH2-new" <${STORE_NAMED_GRAPH}-two> .
+<http://example.com/named-subject> <http://example.com/named-predicate> "named-object-GRAPH1-new" <${STORE_NAMED_GRAPH}-one> .
+<http://example.com/named-subject> <http://example.com/named-predicate> "named-object-GRAPH2-new" <${STORE_NAMED_GRAPH}-three> .
 EOF
 
 for content in n-triples n-quads; do
     for content_type in application/n-triples application/n-quads; do
         for accept_type in application/n-triples application/n-quads; do
-            for graph in none default named; do
+            for graph in none default graph-name-protocol; do
                 for method in GET PUT DELETE POST HEAD PATCH; do
                     case $graph in
                         none)
@@ -21,7 +27,7 @@ for content in n-triples n-quads; do
                         default)
                             graphvar=default
                             ;;
-                        named)
+                        graph-name-protocol)
                             graphvar="graph=${protocolgraph}"
                             ;;
                         *)
@@ -42,18 +48,18 @@ for content in n-triples n-quads; do
                             ;;
                     esac
 
+                    union=$(echo -e "${initial_input}\n${contentvar}")
+                    #echo "${union}"
+                    union_lines=$(echo "${union}" | wc -l)
+
                     echo -n "content: ${content} "
                     echo "content_type: ${content_type} accept_type: ${accept_type} graph: ${graph} method: ${method} graphvar: ${graphvar}"
                     delete_revisions --repository "${STORE_REPOSITORY_WRITABLE}" > /dev/null
-                    curl_graph_store_update -X POST  -o /dev/null \
+                    echo "${initial_input}" | \
+                         curl_graph_store_update -X POST  -o /dev/null \
                                             -H "Content-Type: application/n-quads" \
-                                            --repository "${STORE_REPOSITORY_WRITABLE}" \
-                                            <<EOF
-<http://example.com/default-subject> <http://example.com/default-predicate> "default-object-org" .
-<http://example.com/named-subject> <http://example.com/named-predicate> "named-object-GRAPH1-org" <${STORE_NAMED_GRAPH}> .
-<http://example.com/named-subject> <http://example.com/named-predicate> "named-object-GRAPH2-org" <${STORE_NAMED_GRAPH}-two> .
-<http://example.com/named-subject> <http://example.com/named-predicate> "named-object-GRAPH3-org" ${protocolgraph} .
-EOF
+                                            --repository "${STORE_REPOSITORY_WRITABLE}"
+                    
                     #output=$(curl_graph_store_get -i --repository "${STORE_REPOSITORY_WRITABLE}")
                     #echo "$output" | egrep "^Content-Type: " |cut -d' ' -f2 | sed 's/;//g'
                     #echo -e "contentvar:\n++\n${contentvar}\n--"
@@ -117,7 +123,7 @@ EOF
                         echo "404 NOT FOUND"
                     else
                         output=$(echo "$output"| head -n -1)
-                        response_type=$(echo "$output"| tail -n 1 |cut -d' ' -f1 | sed 's/;//g')
+                        response_type=$(echo "$output"| tail -n 1 | cut -d' ' -f1 | sed 's/;//g')
                         if [ "${response_type}" != "application/n-quads" ]; then
                             echo "wrong response: ${response_type}"
                             exit 2
@@ -147,13 +153,51 @@ EOF
                         fi
                         echo "empty: ${empty} triples: ${triples} quads: ${quads} unknown: ${unknown}"
 
-                        echo "$response" | grep "default-object-new"
-"default-object-new"
-"named-object-GRAPH1-new" <${STORE_NAMED_GRAPH}>
-"named-object-GRAPH2-new" <${STORE_NAMED_GRAPH}-two>
-"named-object-GRAPH1-org" <${STORE_NAMED_GRAPH}>
-"named-object-GRAPH2-org" <${STORE_NAMED_GRAPH}-two>
-"named-object-GRAPH3-org" ${protocolgraph}
+                        # check content
+                        for i in $(seq $union_lines); do
+                            statement=$(echo "${union}" | sed "${i}q;d")
+                            elem=$(echo "${statement}" | awk '{print NF}')
+                            if [ "$elem" -eq 4 ]; then
+                                statement_object=$(echo "${statement}" | cut -d' ' -f3)
+                                statement_graph=default
+                            elif [ "$elem" -eq 5 ]; then
+                                statement_object=$(echo "${statement}" | cut -d' ' -f3)
+                                statement_graph=$(echo "${statement}" | cut -d' ' -f4)
+                            elif [ "$elem" -eq 0 ]; then
+                                echo empty statement
+                                exit 3
+                            else
+                                echo unknown statement type
+                                exit 3
+                            fi
+                            
+                            #echo "looking for ${statement_object} in ${statement_graph}"
+                            #echo -n "            "
+                            found=$(echo "${response}" | grep ${statement_object})
+                            if [ -n "${found}" ]; then
+                                elem=$(echo "${found}" | awk '{print NF}')
+                                if [ "$elem" -eq 4 ]; then
+                                    found_object=$(echo "${found}" | cut -d' ' -f3)
+                                    found_graph=default
+                                elif [ "$elem" -eq 5 ]; then
+                                    found_object=$(echo "${found}" | cut -d' ' -f3)
+                                    found_graph=$(echo "${found}" | cut -d' ' -f4)
+                                elif [ "$elem" -eq 0 ]; then
+                                    echo "empty statement in result"
+                                    exit 3
+                                else
+                                    echo "unknown statement type in result"
+                                    exit 3
+                                fi
+                                if [ "${statement_graph}" = "${found_graph}" ]; then
+                                    echo "${statement_object} found in graph ${statement_graph}"
+                                else
+                                    echo "${statement_object} found in graph ${found_graph} (was in ${statement_graph})"
+                                fi
+                            else
+                                echo "${statement_object} not found"
+                            fi
+                        done
                     fi
                     echo
                     #sed 's/"[^"]*"/STRING/g' | sed 's/<[^>]*>/URL/g' | awk '{print NF}'

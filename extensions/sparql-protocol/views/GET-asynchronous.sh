293507e6cded3a5173a4baa664afd8bce6d93cf2
james anderson <james@dydra.com>
2023-12-24 04:43:05 +0100

    modify output and success criteria in asynchronous view test

--- extensions/sparql-protocol/views/GET-asynchronous.sh
+++ extensions/sparql-protocol/views/GET-asynchronous.sh
@@ -1,17 +1,21 @@
 #! /bin/bash
 
 # test asynchronous view invocation
+# initialize a source repository
 # - clear a target repository
 # - direct the request to a construct view
 # - specify notify-asynchronous
 # - direct the result to a repository with content type ntriples
 # - test for content in the target repository
+#
+# for ((i = 0; i < 10; i ++)) ; do echo -n "${i}: "; run_test /Users/Shared/Development/Source/Library/com/github/dydra/http-api-tests/extensions/sparql-protocol/views/GET-asynchronous.sh ; done
+
 
 function async_sparql_get () {
   viewName="$1"
   requestID=`date +%Y%m%dT%H%M%S`
   echo "${0} async request ${requestID}" >  ${ECHO_OUTPUT}
-  curl_sparql_view -w "%{http_code}\n" -o /dev/null \
+  curl_sparql_view -w "%{http_code}\n" -o  ${ECHO_OUTPUT} \
     -H "Content-Type: " \
     -H "Accept-Asynchronous: notify" \
     -H "Asynchronous-Location: https://${STORE_HOST}/${STORE_ACCOUNT}/${STORE_REPOSITORY_WRITABLE}/service?default" \
@@ -21,7 +25,7 @@ function async_sparql_get () {
     -H "Client-Request-Id: ${requestID}" \
     $viewName \
     --repository "${STORE_REPOSITORY}" \
-   |  fgrep -q 202
+   | fgrep -q 202
 }
 
 # ensure an empty target repository and source content
@@ -41,7 +45,7 @@ sleep 20
 
 # curl_sparql_view --repository ${STORE_REPOSITORY_WRITABLE} all
 
-curl_graph_store_get --repository ${STORE_REPOSITORY_WRITABLE} | sort | cmp -s /dev/stdin GET-asynchronous.nt
+curl_graph_store_get --repository ${STORE_REPOSITORY_WRITABLE} | sort | tee ${ECHO_OUTPUT} | cmp -s /dev/stdin GET-asynchronous.nt
 
 echo "${0} complete" >  ${ECHO_OUTPUT}
 
